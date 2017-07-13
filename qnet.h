ee869d6fb4545da35449761418d2484c2f55a574
Pinguo <pinguo@SurfacePro4.local>
2017-07-13 19:46:52 +0800

    格式和item代码整理

--- qnet.h
+++ qnet.h
@@ -1,6 +1,13 @@
 #ifndef _MQ_NET_H
 #define _MQ_NET_H
+#include <sys/socket.h>
+#include <fcntl.h>
 
+#define QNET_OK 0
+#define QNET_ERR -1
 
+int netSetBlockState(int fd, int state);
+int netFdBlock(int fd);
+int netFdNonBlock(int fd);
 
 #endif
